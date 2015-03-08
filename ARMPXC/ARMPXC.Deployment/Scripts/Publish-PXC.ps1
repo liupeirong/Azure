b01c3f094da5b6d6d0ace90efa6255661a10042f
liupeirong <liupeirong@gmail.com>
2015-03-08 12:36:33 -0700

    loadbalancer ssh

---
+++ ARMPXC/ARMPXC.Deployment/Scripts/Publish-PXC.ps1
@@ -0,0 +1,25 @@
+﻿#Requires -Version 3.0
+
+<#
+What else would you like this script to do for you?  Send us feedback here: http://go.microsoft.com/fwlink/?LinkID=517524 
+#>
+
+Param(
+  [string] $ResourceGroupName = 'PXCRG',
+  [string] $TemplateFile = '..\Templates\PXC.json',
+  [string] $TemplateParametersFile = '..\Templates\PXC.param.dev.json'
+)
+
+Set-StrictMode -Version 3
+#New-AzureResourceGroup -Name $ResourceGroupName -Location 'East US'
+
+# Convert relative paths to absolute paths if needed
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$TemplateParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile)
+
+# Create or update the resource group using the specified template file and template parameters file
+Switch-AzureMode AzureResourceManager
+New-AzureResourceGroupDeployment `
+                       -ResourceGroupName $ResourceGroupName `
+                       -TemplateFile $TemplateFile `
+                       -TemplateParameterFile $TemplateParametersFile
