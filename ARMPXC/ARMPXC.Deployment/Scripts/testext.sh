7b44c3e507f54e6670a5afd53f779c4cfb4fe08e
liupeirong <liupeirong@gmail.com>
2015-03-09 10:45:34 -0700

    convert sh to ansi with linux eol

--- ARMPXC/ARMPXC.Deployment/Scripts/testext.sh
+++ ARMPXC/ARMPXC.Deployment/Scripts/testext.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 # This script is only tested on CentOS 6.5 with Percona XtraDB Cluster 5.6.
 # You can customize variables such as MOUNTPOINT, RAIDCHUNKSIZE and so on to your needs.
@@ -13,3 +13,23 @@ NODENAME=$(hostname)
 
 echo "${CLUSTERADDRESS}, ${NODEADDRESS}, ${NODENAME}" >/tmp/testext.log
 
+open_ports() {
+    iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 4444 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 4567 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 4568 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 9200 -j ACCEPT
+    iptables-save
+}
+
+disable_apparmor() {
+	/etc/init.d/apparmor stop
+	update-rc.d -f apparmor remove
+}
+
+configure_network() {
+    open_ports
+    disable_apparmor
+}
+
+configure_network
