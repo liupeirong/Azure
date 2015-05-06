5b67f8429d78b45e5888524ec03f6b937aa83c33
liupeirong <liupeirong@gmail.com>
2015-05-06 15:37:55 -0700

    update with sas

--- LargeBlobUploader/Program.cs
+++ LargeBlobUploader/Program.cs
@@ -10,14 +10,19 @@
         static void Main(string[] args)
         {
             LargeFileUploaderUtils.Log = Console.Out.WriteLine;
-            LargeFileUploaderUtils.NumBytesPerChunk = 8 * 1024;
+            LargeFileUploaderUtils.NumBytesPerChunk = 2 * 1024 * 1024;
 
-            LargeFileUploaderUtils.UploadAsync(
-                inputFile: @"C:\temp\watestsample.bak",
-                storageConnectionString: Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING", EnvironmentVariableTarget.Machine),
+            //LargeFileUploaderUtils.UploadAsync(
+            //    inputFile: @"C:\temp\watestsample.bak",
+            //    storageConnectionString: Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING", EnvironmentVariableTarget.Machine),
+            //    containerName: "bigfiles",
+            //    uploadParallelism: 2).Wait();
+
+            LargeFileUploaderUtils.UploadAsyncSAS(
+                inputFile: @"C:\temp\Microsoft_Intune_Setup.zip",
+                containerSASString: @"https://schneiderstore.blob.core.windows.net/bigfiles?sv=2012-02-12&sr=c&si=samplePolicy&sig=E7m9f4Udq2ztBrwhSOPmSbcR8G5qu1g39To6WwJ3%2Bus%3D",
                 containerName: "bigfiles",
                 uploadParallelism: 2).Wait();
-
 
             //byte[] someData = Encoding.UTF8.GetBytes("Hallo");
 
