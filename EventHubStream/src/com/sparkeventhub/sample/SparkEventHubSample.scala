623cc8cfa2c98b4114c0882c8da0b8e984355a3c
liupeirong <liupeirong@gmail.com>
2015-11-27 19:43:03 -0800

    Eventhub to Spark streaming and Spark SQL

---
+++ EventHubStream/src/com/sparkeventhub/sample/SparkEventHubSample.scala
@@ -0,0 +1,65 @@
+package com.sparkeventhub.sample
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.eventhubs.EventHubsUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import scala.reflect.runtime.universe
+
+object SparkEventHubSample {
+  def main(args: Array[String]) {
+
+    val outputDir = "sparkoutput/calls.raw"
+    val streamBatchIntervalInSeconds = 60
+    val jdbccxn = "jdbc:sqlserver://e2i92hd2xp.database.windows.net:1433;database=MCCAzure;user=soi@e2i92hd2xp;password=Sp3cial!;encrypt=false;loginTimeout=30;"
+
+    val ehParams = Map[String, String](
+      "eventhubs.policyname" -> "ReceiveRule",
+      "eventhubs.policykey" -> "jSV/BjU6sKSzffEit5ti3s0j0exRhB7XwYPXpGiAZRY=",
+      "eventhubs.namespace" -> "clouderademo",
+      "eventhubs.name" -> "stratahub",
+      "eventhubs.partition.count" -> "2", //executor count must be twice that of partition count
+      "eventhubs.consumergroup" -> "$default",
+      "eventhubs.checkpoint.dir" -> "sparkcheckpoint",
+      "eventhubs.checkpoint.interval" -> "600")
+
+    // ----if spark-shell, comment out the following 2 lines
+    val sparkConf = new SparkConf().setAppName("SparkEventHubSample")
+    val sc = new SparkContext(sparkConf);
+    // ----end if spark-shell
+    val ssc = new StreamingContext(sc, Seconds(streamBatchIntervalInSeconds))
+    val stream = EventHubsUtils.createUnionStream(ssc, ehParams)
+    val lines = stream.map(msg => new String(msg))
+    //lines.print()  //above is all we need to verify we can see each message from EventHub
+
+    val isoformat = new java.text.SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'")
+    def date2long: (String => Long) = (s: String) => try { isoformat.parse(s).getTime() } catch { case _: Throwable => 0 }
+    val sqlfunc = udf(date2long)
+
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+    lines.foreachRDD {
+      rdd =>
+        if (rdd.count() > 0) {
+          val rawdf = sqlContext.jsonRDD(rdd);
+          // convert international time string to unix time for easy calculation
+          val df = rawdf.withColumn("callTime", sqlfunc(rawdf("callrecTime")));
+          // use the min time of this mini batch as the timestamp for the aggregated entry
+          val minTime = df.filter(df("callTime") > 0).select("callTime").first().getLong(0);
+          // real time aggregation by region
+          val callByRegion = df.groupBy("SwitchNum").count().withColumnRenamed("count", "callCount").withColumn("callTimeStamp", lit(minTime));
+          // save real time aggregation to sql azure
+          callByRegion.insertIntoJDBC(jdbccxn, "callByRegion", false);
+          // save to hdfs for impala or hive queries
+          val tb = df.registerTempTable("message");
+          val calls = sqlContext.sql("SELECT callrecTime, SwitchNum, CallingIMSI, CallingNum, CalledNum, callTime from message");
+          calls.save(outputDir, SaveMode.Append)
+        }
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
