917cd5cf3ca0f92afe63ff1bcfae8842d31354fd
liupeirong <liupeirong@gmail.com>
2016-07-02 16:29:25 -0700

    AADDS

---
+++ ClouderaAADDS/scripts/bootstrap-cloudera.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Usage: bootstrap-cloudera-1.0.sh {clusterName} {managment_node} {cluster_nodes} {isHA} {sshUserName} [{sshPassword}]
+
+# Put the command line parameters into named variables
+IPPREFIX=$1
+NAMEPREFIX=$2
+NAMESUFFIX=$3
+MASTERNODES=$4
+DATANODES=$5
+ADMINUSER=$6
+HA=$7
+PASSWORD=$8
+CMUSER=$9
+CMPASSWORD=${10}
+EMAILADDRESS=${11}
+BUSINESSPHONE=${12}
+FIRSTNAME=${13}
+LASTNAME=${14}
+JOBROLE=${15}
+JOBFUNCTION=${16}
+COMPANY=${17}
+MNStartIP=${18}
+DNStartIP=${19}
+
+CLUSTERNAME=$NAMEPREFIX
+
+execname=$0
+
+log() {
+  echo "$(date): [${execname}] $@" 
+}
+
+ManagementNode="${IPPREFIX}${MNStartIP}:${NAMEPREFIX}-mn0.$NAMESUFFIX:${NAMEPREFIX}-mn0"
+mip=$(echo "$ManagementNode" | sed 's/:/ /' | sed 's/:/ /' | cut -d ' ' -f 1)
+
+#Generate IP Addresses for the cloudera setup
+NODES=()
+
+let "NAMEEND=MASTERNODES-1"
+for i in $(seq 1 $NAMEEND)
+do 
+  let "IP=i+MNStartIP"
+  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-mn$i.$NAMESUFFIX:${NAMEPREFIX}-mn$i")
+done
+
+let "DATAEND=DATANODES-1"
+for i in $(seq 0 $DATAEND)
+do 
+  let "IP=i+DNStartIP"
+  NODES+=("$IPPREFIX$IP:${NAMEPREFIX}-dn$i.$NAMESUFFIX:${NAMEPREFIX}-dn$i")
+done
+
+IFS=',';NODE_IPS="${NODES[*]}";IFS=$' \t\n'
+
+wip_string=''
+OIFS=$IFS
+IFS=','
+for x in $NODE_IPS
+do
+  log "Workier IP: $x"
+  line=$(echo "$x" | sed 's/:/ /' | sed 's/:/ /')
+  wip_string+=$(echo "$line" | cut -d ' ' -f 1 | sed 's/$/,/')
+  log "current wip_string is: $wip_string"
+done
+IFS=${OIFS}
+worker_ip=$(echo "${wip_string%?}")
+log "Worker ip to be supplied to next script: $worker_ip"
+log "Adminuser: $ADMINUSER Adminpassword: $PASSWORD"
+log "BEGIN: Starting detached script to finalize initialization"
+log "CMUSER $CMUSER, CMPASSWORD $CMPASSWORD, ${10}"
+sh initialize-cloudera-server.sh "$CLUSTERNAME" " " "$mip" "$worker_ip" $HA $ADMINUSER $PASSWORD $CMUSER $CMPASSWORD $EMAILADDRESS $BUSINESSPHONE $FIRSTNAME $LASTNAME $JOBROLE $JOBFUNCTION $COMPANY>/dev/null 2>&1
+log "END: Detached script to finalize initialization running. PID: $!"
+
