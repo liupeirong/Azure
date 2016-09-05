7548dc5874fd2b6a71bd30eb0e9f15e5a65a3d84
liupeirong <liupeirong@gmail.com>
2016-09-04 18:56:42 -0700

    Multi-tenant Web App that serves embedded power bi reports which connect to the respective tenant's Odata feed.

---
+++ MTPbiOdata/AcmeOData/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AcmeOData.Models;
+using System.Web.OData;
+using System.Web.Http;
+
+namespace AcmeOData.Controllers 
+{
+    public class ProductsController : ODataController
+    {
+        [Authorize]
+        public IQueryable<Product> Get()
+        {
+            var products = new List<Product>();
+            products.Add(new Product { Id = 1, Name = "banana", Price = 10, Category = "Fruits" });
+            products.Add(new Product { Id = 2, Name = "blueberry", Price = 20, Category = "Fruits" });
+            products.Add(new Product { Id = 3, Name = "strawberry", Price = 30, Category = "Fruits" });
+            return products.AsQueryable();
+        }
+    }
+}
---
+++ MTPbiOdata/ContosoOData/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ContosoOData.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ContosoOData
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
