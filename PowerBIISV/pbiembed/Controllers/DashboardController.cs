2ec13245e5f78a1e99e8ecb33b724e6feaaf6532
liupeirong <liupeirong@gmail.com>
2016-10-30 19:11:43 -0700

    show list of reports

--- PowerBIISV/pbiembed/Controllers/DashboardController.cs
+++ PowerBIISV/pbiembed/Controllers/DashboardController.cs
@@ -10,6 +10,7 @@ using ISVWebApp.Models;
 using System.Configuration;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using Microsoft.PowerBI.Api.V1.Models;
 
 namespace ISVWebApp.Controllers
 {
@@ -20,6 +21,7 @@ namespace ISVWebApp.Controllers
         private readonly string workspaceId;
         private readonly string accessKey;
         private readonly string apiUrl;
+        private readonly string adminTenant;
 
         public DashboardController()
         {
@@ -27,18 +29,29 @@ namespace ISVWebApp.Controllers
             this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
             this.accessKey = ConfigurationManager.AppSettings["powerbi:AccessKey"];
             this.apiUrl = ConfigurationManager.AppSettings["powerbi:ApiUrl"];
+            this.adminTenant = ConfigurationManager.AppSettings["AdminTenant"];
         }
 
         public ActionResult Reports()
         {
+            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+
             using (var client = this.CreatePowerBIClient())
             {
-                var reportsResponse = client.Reports.GetReports(this.workspaceCollection, this.workspaceId);
+                var viewModel = new ReportsViewModel();
 
-                var viewModel = new ReportsViewModel
+                var reportsResponse = client.Reports.GetReports(this.workspaceCollection, this.workspaceId);
+                if (tenantID != adminTenant)
                 {
-                    Reports = reportsResponse.Value.ToList()
-                };
+                    string tenantReportName = Startup.tenantODataMap[tenantID][1];
+                    var report = reportsResponse.Value.FirstOrDefault(r => r.Name == tenantReportName);
+                    viewModel.Reports = new List<Microsoft.PowerBI.Api.V1.Models.Report>();
+                    viewModel.Reports.Add(report); 
+                }
+                else
+                {
+                    viewModel.Reports = reportsResponse.Value.ToList();
+                }
 
                 return View(viewModel);
             }
@@ -46,13 +59,15 @@ namespace ISVWebApp.Controllers
 
         public async Task<ActionResult> Report(string reportId = "")
         {
-            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
-            string reportName = Startup.tenantODataMap[tenantID][1];
+            //string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            //string reportName = Startup.tenantODataMap[tenantID][1];
             using (var client = this.CreatePowerBIClient())
             {
                 var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, this.workspaceId);
-                var report = reportsResponse.Value.FirstOrDefault(r => (r.Id == reportId || r.Name == reportName));
-                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, report.Id);
+                //var report = reportsResponse.Value.FirstOrDefault(r => (r.Id == reportId || r.Name == reportName));
+                //var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, report.Id);
+                var report = reportsResponse.Value.FirstOrDefault(r => r.Id == reportId);
+                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, reportId);
 
                 var viewModel = new ReportViewModel
                 {
