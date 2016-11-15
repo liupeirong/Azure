16794a3be182f199bb75f9b77d25eb4839a6bb31
liupeirong <liupeirong@gmail.com>
2016-11-15 11:48:41 -0800

    bootstrap disks

---
+++ VMSSMD/bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# input 1: username for admin user
+# input 2: node type (master, worker)
+
+ADMINUSER=$1
+NODETYPE=$2
+
+# Disable the need for a tty when running sudo and allow passwordless sudo for the admin user
+sed -i '/Defaults[[:space:]]\+!*requiretty/s/^/#/' /etc/sudoers
+echo "$ADMINUSER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Mount and format the attached disks base on node type
+if [ "$NODETYPE" == "master" ]
+then
+  bash ./prepare-masternode-disks.sh
+elif [ "$NODETYPE" == "data" ]
+then
+  bash ./prepare-datanode-disks.sh
+else
+  echo "#unknown type, default to datanode"
+  bash ./prepare-datanode-disks.sh
+fi
+
+setenforce 0 >> /tmp/setenforce.out
+cat /etc/selinux/config > /tmp/beforeSelinux.out
+sed -i 's^SELINUX=enforcing^SELINUX=disabled^g' /etc/selinux/config || true
+cat /etc/selinux/config > /tmp/afterSeLinux.out
+
+/etc/init.d/iptables save
+/etc/init.d/iptables stop
+chkconfig iptables off
+
+yum install -y ntp
+service ntpd start
+service ntpd status
+chkconfig ntpd on
+
+echo never | tee -a /sys/kernel/mm/transparent_hugepage/enabled
+echo "echo never | tee -a /sys/kernel/mm/transparent_hugepage/enabled" | tee -a /etc/rc.local
+echo vm.swappiness=1 | tee -a /etc/sysctl.conf
+echo 1 | tee /proc/sys/vm/swappiness
+ifconfig -a >> initialIfconfig.out; who -b >> initialRestart.out
+
+echo net.ipv4.tcp_timestamps=0 >> /etc/sysctl.conf
+echo net.ipv4.tcp_sack=1 >> /etc/sysctl.conf
+echo net.core.rmem_max=4194304 >> /etc/sysctl.conf
+echo net.core.wmem_max=4194304 >> /etc/sysctl.conf
+echo net.core.rmem_default=4194304 >> /etc/sysctl.conf
+echo net.core.wmem_default=4194304 >> /etc/sysctl.conf
+echo net.core.optmem_max=4194304 >> /etc/sysctl.conf
+echo net.ipv4.tcp_rmem="4096 87380 4194304" >> /etc/sysctl.conf
+echo net.ipv4.tcp_wmem="4096 65536 4194304" >> /etc/sysctl.conf
+echo net.ipv4.tcp_low_latency=1 >> /etc/sysctl.conf
+sed -i "s/defaults        1 1/defaults,noatime        0 0/" /etc/fstab
+
