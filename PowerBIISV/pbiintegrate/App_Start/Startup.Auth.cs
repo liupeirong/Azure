bde8ecb58caa4a61dc052faad7a05b9e203e879b
liupeirong <liupeirong@gmail.com>
2017-01-22 14:00:58 -0800

    Replace default token cache with appropriate per user token cache

--- PowerBIISV/pbiintegrate/App_Start/Startup.Auth.cs
+++ PowerBIISV/pbiintegrate/App_Start/Startup.Auth.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Globalization;
-using System.Linq;
 using System.Web;
 using Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.IdentityModel.Claims;
 using System.Threading.Tasks;
+using pbiintegrate.Models;
 
 namespace pbiintegrate
 {
     public partial class Startup
     {
-        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
-        private static string clientKey = ConfigurationManager.AppSettings["ida:ClientKey"];
+        public static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        public static string clientKey = ConfigurationManager.AppSettings["ida:ClientKey"];
         public static string resourceId = ConfigurationManager.AppSettings["ida:ResourceId"];
-        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        public static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
         private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
         private static string authority = aadInstance + tenantId;
-        public static string pbieKey = ConfigurationManager.AppSettings["pbi:EmbedKey"];
 
         public void ConfigureAuth(IAppBuilder app)
         {
@@ -45,9 +44,12 @@ namespace pbiintegrate
                         AuthorizationCodeReceived = async (context) =>
                         {
                             var code = context.Code;
+
+                            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
                             ClientCredential credential = new ClientCredential(clientId, clientKey);
-                            AuthenticationContext authContext = new AuthenticationContext(authority, TokenCache.DefaultShared);
-                            AuthenticationResult authResult = await authContext.AcquireTokenByAuthorizationCodeAsync(
+                            AuthenticationContext authContext = new AuthenticationContext(authority, new ADALTokenCache(signedInUserID));
+                            AuthenticationResult result = await authContext.AcquireTokenByAuthorizationCodeAsync(
                                 code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, resourceId);
                         },
                         RedirectToIdentityProvider = (context) =>
@@ -55,7 +57,6 @@ namespace pbiintegrate
                             // This ensures that the address used for sign in and sign out is picked up dynamically from the request
                             // this allows you to deploy your app (to Azure Web Sites, for example)without having to change settings
                             // Remember that the base URL of the address used here must be provisioned in Azure AD beforehand.
-                            TokenCache.DefaultShared.Clear();
                             string appBaseUrl = context.Request.Scheme + "://" + context.Request.Host + context.Request.PathBase;
                             context.ProtocolMessage.RedirectUri = appBaseUrl + "/";
                             context.ProtocolMessage.PostLogoutRedirectUri = appBaseUrl + "/Account/SignOutCallback";
@@ -64,7 +65,6 @@ namespace pbiintegrate
                         AuthenticationFailed = (context) =>
                         {
                             // Pass in the context back to the app
-                            TokenCache.DefaultShared.Clear();
                             context.OwinContext.Response.Redirect("/Home/Error");
                             context.HandleResponse(); // Suppress the exception
                             return Task.FromResult(0);
--- PowerBIISV/pbiintegrate/Controllers/HomeController.cs
+++ PowerBIISV/pbiintegrate/Controllers/HomeController.cs
@@ -31,65 +31,65 @@ namespace pbiintegrate.Controllers
         }
 
         [Authorize]
-        public ActionResult LOBReport()
+        public async Task<ActionResult> LOBReport()
         {
-            //if a user is already signed in but didn't go through the process of getting authorization code in auth config,
-            //then GetAccessTokenAsync will succeed, but the returned access token can't be used to access the resource.
-            //we have to ask the user to sign out and sign in again to get the authorization code first then access token.
-            if (TokenCache.DefaultShared.Count > 0)
+            ViewBag.accessToken = await GetTokenForApplication(); 
+            string responseContent = string.Empty;
+            string reportsUri = "https://api.powerbi.com/v1.0/myorg/reports";
+
+            System.Net.WebRequest request = System.Net.WebRequest.Create(reportsUri) as System.Net.HttpWebRequest;
+            request.Method = "GET";
+            request.ContentLength = 0;
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", ViewBag.AccessToken));
+
+            using (var response = request.GetResponse() as System.Net.HttpWebResponse)
             {
-                IEnumerable<TokenCacheItem> tokens = TokenCache.DefaultShared.ReadItems();
-                ViewBag.accessToken = tokens.First().AccessToken;
-                string responseContent = string.Empty;
-                string reportsUri = "https://api.powerbi.com/v1.0/myorg/reports";
-
-                System.Net.WebRequest request = System.Net.WebRequest.Create(reportsUri) as System.Net.HttpWebRequest;
-                request.Method = "GET";
-                request.ContentLength = 0;
-                request.Headers.Add("Authorization", String.Format("Bearer {0}", ViewBag.AccessToken));
-
-                using (var response = request.GetResponse() as System.Net.HttpWebResponse)
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
                 {
-                    using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
-                    {
-                        responseContent = reader.ReadToEnd();
-                        PBIReports Reports = JsonConvert.DeserializeObject<PBIReports>(responseContent);
-                        ViewBag.Reports = new SelectList(Reports.value.OrderBy(t => t.name), "embedUrl", "name");
-                    }
+                    responseContent = reader.ReadToEnd();
+                    PBIReports Reports = JsonConvert.DeserializeObject<PBIReports>(responseContent);
+                    ViewBag.Reports = new SelectList(Reports.value.OrderBy(t => t.name), "embedUrl", "name");
                 }
             }
             return View();
         }
 
-        public ActionResult LOBDashboard()
+        [Authorize]
+        public async Task<ActionResult> LOBDashboard()
         {
-            //if a user is already signed in but didn't go through the process of getting authorization code in auth config,
-            //then GetAccessTokenAsync will succeed, but the returned access token can't be used to access the resource.
-            //we have to ask the user to sign out and sign in again to get the authorization code first then access token.
-            if (TokenCache.DefaultShared.Count > 0)
+            ViewBag.accessToken = await GetTokenForApplication();
+            string responseContent = string.Empty;
+            string dashboardsUri = "https://api.powerbi.com/beta/myorg/dashboards";
+
+            WebRequest request = WebRequest.Create(dashboardsUri) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentLength = 0;
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", ViewBag.AccessToken));
+
+            using (var response = request.GetResponse() as System.Net.HttpWebResponse)
             {
-                IEnumerable<TokenCacheItem> tokens = TokenCache.DefaultShared.ReadItems();
-                ViewBag.accessToken = tokens.First().AccessToken;
-                string responseContent = string.Empty;
-                string dashboardsUri = "https://api.powerbi.com/beta/myorg/dashboards";
-
-                WebRequest request = WebRequest.Create(dashboardsUri) as HttpWebRequest;
-                request.Method = "GET";
-                request.ContentLength = 0;
-                request.Headers.Add("Authorization", String.Format("Bearer {0}", ViewBag.AccessToken));
-
-                using (var response = request.GetResponse() as System.Net.HttpWebResponse)
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
                 {
-                    using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
-                    {
-                        responseContent = reader.ReadToEnd();
-                        PBIDashboards Dashboards = JsonConvert.DeserializeObject<PBIDashboards>(responseContent);
-                        ViewBag.Dashboards = new SelectList(Dashboards.value.OrderBy(t => t.displayName), "embedUrl", "displayName");
-                    }
+                    responseContent = reader.ReadToEnd();
+                    PBIDashboards Dashboards = JsonConvert.DeserializeObject<PBIDashboards>(responseContent);
+                    ViewBag.Dashboards = new SelectList(Dashboards.value.OrderBy(t => t.displayName), "embedUrl", "displayName");
                 }
             }
             return View();
         }
 
+        public async Task<string> GetTokenForApplication()
+        {
+            string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            // get a token for the Graph without triggering any user interaction (from the cache, via multi-resource refresh token, etc)
+            ClientCredential clientcred = new ClientCredential(Startup.clientId, Startup.clientKey);
+            // initialize AuthenticationContext with the token cache of the currently signed in user, as kept in the app's database
+            AuthenticationContext authenticationContext = new AuthenticationContext(Startup.aadInstance + tenantID, new ADALTokenCache(signedInUserID));
+            AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenSilentAsync(Startup.resourceId, clientcred, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+            return authenticationResult.AccessToken;
+        }
     }
 }
