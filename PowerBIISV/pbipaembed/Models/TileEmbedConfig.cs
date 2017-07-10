bcc09d2286f86b1c46a5a7da8602c1dde46bf95f
liupeirong <liupeirong@gmail.com>
2017-07-09 19:24:15 -0700

    Power BI Premium Embed for non-Power BI users

---
+++ PowerBIISV/pbipaembed/Models/TileEmbedConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.PowerBI.Api.V2.Models;
+using System;
+
+namespace pbipaembed.Models
+{
+    public class TileEmbedConfig : EmbedConfig
+    {
+        public string dashboardId { get; set; }
+
+    }
+}
