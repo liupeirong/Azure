5b01d0be95a180b705fcdb9ce2640d5aae598d3a
liupeirong <liupeirong@gmail.com>
2017-10-08 20:17:01 -0700

    Oauth2 protected Odata API

---
+++ PowerBIRest/Oauth2Odata/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Oauth2Odata
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
