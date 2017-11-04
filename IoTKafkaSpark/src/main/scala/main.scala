81ecdc9a0736a4ad72adb36261e767f9543d4e63
paige <liupeirong@gmail.com>
2017-11-04 22:44:04 +0000

    csv and json support

--- IoTKafkaSpark/src/main/scala/main.scala
+++ IoTKafkaSpark/src/main/scala/main.scala
@@ -1,8 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.streaming.Trigger
 import scala.concurrent.duration._
 import com.typesafe.config._
 
@@ -15,69 +13,38 @@ object IoTStreaming {
     val workingDir = appconf.getString("iotsim.devicelogWorkingDir")
     val devicelogDir = appconf.getString("iotsim.devicelogDir")
     val devicelogCheckpointDir = appconf.getString("iotsim.devicelogCheckpointDir")
+    val messageFormat = appconf.getString("iotsim.messageFormat")
     
     val spark = SparkSession
       .builder
       .appName("iotsim")
       .getOrCreate
     import spark.implicits._
-      
+    
+    // executors and executor cores vs partition
     val dfraw = spark.readStream.
       format("kafka").
       option("kafka.bootstrap.servers", kafkaBrokers).
-      option("subscribe", kafkaTopic).
+      option("subscribe", kafkaTopic). 
+      //no need to set consumer group, a unique group will be auto created
+      //each partition will be assigned to one consumer in the group, each consumer can consume multiple partitions
+      //when the number of consumers equals to the number of kafka partitions, you achieve max parallelism without idle
+      //stick with about 5 cores per executor
       option("startingOffsets", "earliest").
-      option("maxOffsetsPerTrigger", maxOffsetsPerTrigger).
+      option("maxOffsetsPerTrigger", maxOffsetsPerTrigger).  //this controls how many messages to read per trigger
       load()
-    //for testing, instead of Kafka, use file source
+      
+    //for testing, use file source instead of Kafka
     //val dfraw = spark.readStream.schema(devicelogSchema).option("header", "true").csv("/user/pliu/iotinput")
 
-    /* if CSV */
-    val devicelogSchema = new StructType().
-      add("deviceid", StringType).
-      add("cycle", IntegerType).
-      add("counter",IntegerType).
-      add("endofcycle",IntegerType).
-      add("sensor9",FloatType).
-      add("sensor11",FloatType).
-      add("sensor14",FloatType).
-      add("sensor15",FloatType)
-    /* end if CSV */
-    
-    //TODO: type CSV fields, and add csv/json converter
-      
-    /* if JSON */
-    //using JSON parser to convert data types is not reliable,so we keep everything as string 
-    //see http://blog.antlypls.com/blog/2016/01/30/processing-json-data-with-sparksql/
-    val devicelogSchemaJ = new StructType().
-      add("deviceid", StringType).
-      add("cycle", StringType).
-      add("counter",StringType).
-      add("endofcycle",StringType).
-      add("sensor9",StringType).
-      add("sensor11",StringType).
-      add("sensor14",StringType).
-      add("sensor15",StringType)
-    val dfj = dfraw.select(from_json($"value".cast(StringType), devicelogSchemaJ).alias("jsonval"))
-    
-    //use Spark SQL to convert to the right data types 
-    val dftyped = dfj.select(
-        $"jsonval.deviceid".cast(StringType).alias("deviceid"),
-        $"jsonval.cycle".cast(IntegerType).alias("cycle"),
-        $"jsonval.counter".cast(IntegerType).alias("counter"),
-        $"jsonval.endofcyle".cast(IntegerType).alias("endofcycle"),
-        $"jsonval.sensor9".cast(FloatType).alias("sensor9"),
-        $"jsonval.sensor11".cast(FloatType).alias("sensor11"),
-        $"jsonval.sensor14".cast(FloatType).alias("sensor14"),
-        $"jsonval.sensor15".cast(FloatType).alias("sensor15")
-        )
-    /* end if JSON */    
-        
+    val dftyped = if (messageFormat == "csv") toTypedDF.fromCSV(dfraw, spark) else toTypedDF.fromJSON(dfraw, spark)
+
     // if the events include a timestamp field
     //val df = dftyped.withColumn("ts", from_unixtime($"ts" /1000, "YYYY-MM-dd HH:mm:ss").cast(TimestampType))
     // else we add a timestamp field just to show how to use the windowing functions below    
     val df = dftyped.withColumn("ts", current_timestamp)
     
+    /* aggregation */
     val dfagg = df.
       withWatermark("ts", "1 minute").
       groupBy(window($"ts", "1 minute"), $"deviceid").
@@ -85,23 +52,33 @@ object IoTStreaming {
       select($"window.start", $"window.end", $"deviceid", $"count").
       withColumn("year", year($"ts")).
       withColumn("month", month($"ts"))
-        
+    
+    /* alerting - not used in this example */
+    //spark.conf.get("spark.sql.caseSensitive") by default its false
+    val dfalert = df.
+    	filter($"tag" === "temperature" && $"sensor11" > 600).
+    	withColumn("message", concat(lit("temperature too high"),$"sensor11"))
+
+    /* storing output */
+    // the working folder is partitioned by year and month, so after the month ends, 
+    // run the compaction job to compact the files in that month of the year to its destination folder
     val query = dfagg.
       writeStream.
       format("parquet").
       partitionBy("year", "month").
+      trigger(Trigger.ProcessingTime(1.minutes)). //trigger controls how often to read from Kafka
       option("path", workingDir).
-      option("checkpointLocation",devicelogCheckpointDir).
-      start
-
-    query.awaitTermination
-      
+      option("checkpointLocation",devicelogCheckpointDir).  //checkpoint controls offset to read from
+      start()
+     
     // for testing, use console sink. Note that only Append mode is supported for file sink. Append mode only works 
     // with watermark, and will only produce outputs when the next trigger kicks in AND
     // max seen event time - watermark > evaluated time window, so by default it's append mode
     //    val query = dfagg.writeStream.format("console").option("truncate", false).start 
     // if you don't see result, try Update or Complete mode
     //    val query = dfagg.writeStream.format("console").outputMode(OutputMode.Update).option("truncate", false).start
+
+    query.awaitTermination
 
     /* compacting small files in working dir to larger files*/
     /*
@@ -112,16 +89,6 @@ object IoTStreaming {
       parquet(devicelogDir + "/year=2017/month=10")
     */
             
-    /* alerting */
-    /*
-    //spark.conf.get("spark.sql.caseSensitive") by default its false
-    import org.apache.spark.sql.functions.{concat, lit}
-    val dfalert = df.
-    	filter($"tag" === "temperature" && $"sensor11" > 600).
-    	withColumn("message", concat(lit("temperature too high"),$"sensor11"))
-
-    val queryalert = dfalert.writeStream.format("console").option("truncate", false).start
-    */    
     //TODO use Kafka console producer to simulate device
     //TODO create hive tables on parquet, all partitioned fields must be lower case
   }  
---
+++ IoTKafkaSpark/src/main/scala/toTypedDF.scala
@@ -0,0 +1,55 @@
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+object toTypedDF {
+  def fromCSV(dfraw: DataFrame, spark: SparkSession): DataFrame = {
+    val delim = ","
+    val nullvalue = "NULL"
+    
+    import spark.implicits._ 
+    val dfv = dfraw.select($"value".cast(StringType).alias("val"))
+    dfv.withColumn("_tmp", split($"val", delim)).select(
+        $"_tmp".getItem(0).cast(StringType).alias("deviceid"),
+        $"_tmp".getItem(1).cast(IntegerType).alias("cycle"),
+        $"_tmp".getItem(2).cast(IntegerType).alias("counter"),
+        $"_tmp".getItem(3).cast(IntegerType).alias("endofcycle"),
+        $"_tmp".getItem(4).cast(FloatType).alias("sensor9"),
+        $"_tmp".getItem(5).cast(FloatType).alias("sensor11"),
+        $"_tmp".getItem(6).cast(FloatType).alias("sensor14"),
+        $"_tmp".getItem(7).cast(FloatType).alias("sensor15")
+        ).
+        //if we have a nullable string value, make sure to convert it to null,capital "NULL" causes parsing failure
+        //withColumn("stringvalue", regexp_replace(col("stringvalue"), nullvalue, null)).
+        drop("_tmp")
+  }
+  
+  def fromJSON(dfraw: DataFrame, spark: SparkSession): DataFrame = {
+    //using JSON parser to convert data types is not reliable,so we keep everything as string 
+    //see http://blog.antlypls.com/blog/2016/01/30/processing-json-data-with-sparksql/
+   import spark.implicits._ 
+   val devicelogSchemaJ = new StructType().
+      add("deviceid", StringType).
+      add("cycle", StringType).
+      add("counter",StringType).
+      add("endofcycle",StringType).
+      add("sensor9",StringType).
+      add("sensor11",StringType).
+      add("sensor14",StringType).
+      add("sensor15",StringType)
+      
+    val dfj = dfraw.select(from_json($"value".cast(StringType), devicelogSchemaJ).alias("jsonval"))
+    
+    //use Spark SQL to convert to the right data types 
+    dfj.select(
+        $"jsonval.deviceid".cast(StringType).alias("deviceid"),
+        $"jsonval.cycle".cast(IntegerType).alias("cycle"),
+        $"jsonval.counter".cast(IntegerType).alias("counter"),
+        $"jsonval.endofcyle".cast(IntegerType).alias("endofcycle"),
+        $"jsonval.sensor9".cast(FloatType).alias("sensor9"),
+        $"jsonval.sensor11".cast(FloatType).alias("sensor11"),
+        $"jsonval.sensor14".cast(FloatType).alias("sensor14"),
+        $"jsonval.sensor15".cast(FloatType).alias("sensor15")
+        )
+  }
+}
