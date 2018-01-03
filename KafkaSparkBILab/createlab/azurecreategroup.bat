d68583b09ce1539f0ae1293c8db0a5533c81d6f1
liupeirong <liupeirong@gmail.com>
2018-01-02 21:13:00 -0800

    same group difference cluster name

--- KafkaSparkBILab/createlab/azurecreategroup.bat
+++ KafkaSparkBILab/createlab/azurecreategroup.bat
@@ -1,8 +1,9 @@
 echo off
 
 set location=%1
-set group=%2
+set clusterName=%2
+set group="ill301rg"
 
 echo on
 
-az group create -l %location% -n %group% && az group deployment create --resource-group %group% --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
+az group create -l %location% -n %group% && az group deployment create --resource-group %group% --template-file azuredeploy.json --parameters clusterName=%clusterName%
