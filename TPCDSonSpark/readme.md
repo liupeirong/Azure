de49fbe531320cd70ed73084e4ff209470a72617
liupeirong <liupeirong@gmail.com>
2018-01-31 17:50:05 -0800

    how to run tpcds

---
+++ TPCDSonSpark/readme.md
@@ -0,0 +1,51 @@
+# Running TPCDS on Spark #
+
+TPCDS has been modified to run on Spark as a Spark SQL performance tests by Databricks.  You can run either existing notebooks in Databricks, or from scratch in your own Spark environment.  The following instructions describe steps to take in both cases.  These steps have been tested on a Cloudera cluster with Spark 2.2.0, an HDInsight cluster with Spark 2.1.1, and Azure Databricks with Spark 2.2.1.  
+
+### To run TPCDS in your own Spark environment
+1. On every Spark node, clone and build [Databricks TPCDS kit](https://github.com/databricks/tpcds-kit) as stated in the readme of that repo. Note that you must ```git clone https://github.com/databricks/tpcds-kit.git```, not https://github.com/__databricks__/tpcds-kit.git as stated in the readme of that repo. 
+
+2. On the Spark driver, clone an build [Databricks Spark SQL Performance tools](https://github.com/databricks/spark-sql-perf):
+    * ```git clone https://github.com/databricks/spark-sql-perf.git```
+    * ```bin/run --benchmark DatasetPerformance```
+    * ```build/sbt package``` this will build the jar file in the target folder
+
+3. Generate TPCDS data
+    * You can generate data in different formats. Parquet is generally recommended. If you create temporary tables, Hive is not required. However, if you create external tables, Hive is required. 
+        * Hive2 is required to support Parquet. As of this writing (CDH5.14) comes with Hive1. So you need to use another format such as ORC.
+        * In Cloudera, you must enable "Hive Service" for Spark2.
+    * Data is generated in an HDFS folder, not local file system.
+    * If you are comparing performance in different Spark environments, make sure data is generated with the same parameters, for example, the following parameters to the genData function must match in each environment: 
+```
+format
+partitionTables
+clusterByPartitionColumns
+filterOutNullPartitionValues
+numPartitions
+```
+    * Follow the [readme in the repo]https://github.com/databricks/spark-sql-perf) or [this scala file](/TPCDSonSpark/run_tpcds.scala) to generate the data.
+
+4. Run TPCDS queries
+    * Add the following package and jar to Spark command line, the jar is the one you built in step 2 above:
+```
+--packages com.typesafe.scala-logging:scala-logging-slf4j_2.10:2.1.2 --jars /path-to/spark-sql-perf_2.11-0.5.0-SNAPSHOT.jar
+```
+    * If you are comparing performance in different Spark environments, make sure Spark and Java settings are as same as possible, for example:
+```
+--conf spark.executor.memory=7g 
+--conf spark.driver.maxResultSize=4g 
+--conf spark.driver.memory=7g 
+--num-executors=1 
+--conf spark.executor.extraJavaOptions="-XX:ReservedCodeCacheSize=256m -XX:+UseCodeCacheFlushing -Xss4m"
+```
+    * If you are comparing performance in difference Spark environments, make sure parameters to runExperiment function are same.
+    * Follow the [readme in the repo]https://github.com/databricks/spark-sql-perf) or [this scala file](/TPCDSonSpark/run_tpcds.scala) to run the tests.
+
+5. Get TPCDS results
+    * experiment.getCurrentResults documented in [readme]https://github.com/databricks/spark-sql-perf) is not the same format as the results written to HDFS. If you want to look at the results from the result file written to HDFS, follow the code in [this scala file](/TPCDSonSpark/run_tpcds.scala). 
+    
+### To run TPCDS in databricks
+1. Import [tpcds_datagen notebook](https://github.com/databricks/spark-sql-perf/blob/master/src/main/notebooks/tpcds_datagen.scala). This notebook takes care of installing tpcds-kit on every worker node. If you are comparing performance in different Spark environments, make sure all data gen parameters are same as described in Step 3 in the previous section. 
+2. Import [tpcds_run notebook](https://github.com/databricks/spark-sql-perf/blob/master/src/main/notebooks/tpcds_run.scala). If you are comparing performance in different Spark environments, make sure all parameters are same as described in Step 4 in the previous section.
+
+
---
+++ TPCDSonSpark/run_tpcds.scala
@@ -0,0 +1,116 @@
+//
+// This file combines generating data, running tests, and retrieving test results in a single spark-shell run.  
+// you can certainly selectively run separate parts.
+// A sample command line will be: 
+// spark-shell --packages com.typesafe.scala-logging:scala-logging-slf4j_2.10:2.1.2 --jars /path/to/spark-sql-perf_2.11-0.5.0-SNAPSHOT.jar --conf spark.executor.memory=7g 
+//   --conf spark.driver.maxResultSize=4g --conf spark.driver.memory=7g --num-executors=1 
+//   --conf spark.executor.extraJavaOptions="-XX:ReservedCodeCacheSize=256m -XX:+UseCodeCacheFlushing -Xss4m"
+//
+
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+/////////////////////////////////
+// Generate data
+/////////////////////////////////
+val databaseName="tpcds"
+val rootDir="/path/to/hdfs/tpcdsdata"
+val tpcdskitDir="/path/to/local/tpcds-kit/tools"
+
+// data gen parameters
+val scaleFactor="1"
+val format="orc"
+val useDecimal = false 
+val useDate = false
+val filterNull = false
+val shuffle = true
+val dsdgen_nonpartitioned = 10
+val dsdgen_partitioned = 10000
+sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")
+// TPCDS has around 2000 dates.
+spark.conf.set("spark.sql.shuffle.partitions", "2000")
+// Don't write too huge files.
+sqlContext.setConf("spark.sql.files.maxRecordsPerFile", "20000000")
+
+import com.databricks.spark.sql.perf.tpcds.TPCDSTables
+val tables = new TPCDSTables(sqlContext,dsdgenDir=tpcdskitDir, scaleFactor = scaleFactor, useDoubleForDecimal = useDecimal, useStringForDate = useDate)
+
+// generate small tables
+val nonPartitionedTables = Array("call_center", "catalog_page", "customer", "customer_address", "customer_demographics", "date_dim", "household_demographics", "income_band", "item", "promotion", "reason", "ship_mode", "store",  "time_dim", "warehouse", "web_page", "web_site")
+nonPartitionedTables.foreach { t => {
+  tables.genData(
+      location = rootDir,
+      format = format,
+      overwrite = true,
+      partitionTables = true,
+      clusterByPartitionColumns = shuffle,
+      filterOutNullPartitionValues = filterNull,
+      tableFilter = t,
+      numPartitions = dsdgen_nonpartitioned)
+  println("Done generating non partitioned tables.")
+}}
+
+// leave the biggest/potentially hardest tables to be generated last.
+val partitionedTables = Array("inventory", "web_returns", "catalog_returns", "store_returns", "web_sales", "catalog_sales", "store_sales") 
+partitionedTables.foreach { t => {
+  tables.genData(
+      location = rootDir,
+      format = format,
+      overwrite = true,
+      partitionTables = true,
+      clusterByPartitionColumns = shuffle,
+      filterOutNullPartitionValues = filterNull,
+      tableFilter = t,
+      numPartitions = dsdgen_partitioned)
+  println("Done generating partitioned tables.")
+}}
+
+// Create the specified database
+sql(s"drop database if exists $databaseName cascade")
+sql(s"create database $databaseName") //on HDInsight, specify location '/path/to/hdfs/db', otherwise, it uses current local folder name
+sql(s"use $databaseName")
+tables.createExternalTables(rootDir, format, databaseName, overwrite = true, discoverPartitions = true)
+// If you don't have Hive, or Hive 2 which supports parquet, you can create temporary tables which only exists in this session
+//tables.createTemporaryTables(rootDir, format)
+
+/////////////////////////////////
+// Run tests
+/////////////////////////////////
+import com.databricks.spark.sql.perf.tpcds.TPCDS
+val tpcds = new TPCDS (sqlContext = sqlContext)
+
+// test run parameters
+val resultLocation = "/path/to/hdfs/tpcdsresult"
+val iterations = 1 // how many iterations of queries to run.
+val queries = tpcds.tpcds2_4Queries // queries to run.
+val timeout = 24*60*60 // timeout, in seconds.
+spark.conf.set("spark.sql.broadcastTimeout", "10000") // good idea for Q14, Q88.
+
+sql(s"use $databaseName")
+val experiment = tpcds.runExperiment(
+  queries, 
+  iterations = iterations,
+  resultLocation = resultLocation,
+  forkThread = true)
+experiment.waitForFinish(timeout)
+
+/////////////////////////////////
+// Retrieve Results
+/////////////////////////////////
+
+// if we are still in the current session
+import org.apache.spark.sql.functions.{col, lit, substring, explode}
+val results = experiment.getCurrentResults.
+  withColumn("Name", substring(col("name"), 2, 100)).
+  withColumn("Runtime", (col("parsingTime") + col("analysisTime") + col("optimizationTime") + col("planningTime") + col("executionTime")) / 1000.0).
+  select('Name, 'Runtime)
+
+// if we are looking at the results in the results file  
+val results = spark.read.json(resultLocation).  //.filter(timestamp = 12345)
+  select(explode($"results")).
+  withColumn("Name", substring($"col.name", 2, 100)).
+  withColumn("RunTime", ($"col.parsingTime" + $"col.analysisTime" + $"col.optimizationTime" + $"col.planningTime" + $"col.executionTime") / 1000.0).
+  select('Name, 'Runtime)
+  
+results.show
+results.collect.foreach(println)
+results.repartition(1).write.format("csv").save("/path/to/tpcdsresult.csv")
