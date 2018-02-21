3e35472ca0c967647d1db2faeebe22d307abf6d9
liupeirong <liupeirong@gmail.com>
2018-02-21 09:45:05 -0800

    try read_from_head

--- VMMonitoring/setup-fluentd.sh
+++ VMMonitoring/setup-fluentd.sh
@@ -25,6 +25,7 @@ cat >> /etc/td-agent/td-agent.conf <<EOF
   azure_storage_type       blob
   store_as                 gzip
   auto_create_container    true
+  read_from_head           true
   path                     logs/
   azure_object_key_format  %{path}%{time_slice}_%{index}.%{file_extension}
   time_slice_format        %Y%m%d%H%M
