4f8d61f6c990bfbc8ac54a7f98142b48d29979e6
liupeirong <liupeirong@gmail.com>
2018-06-14 13:09:48 -0700

    remove unused packages

--- StorageWithMSI/src/main/java/com/paige/azure/App.java
+++ StorageWithMSI/src/main/java/com/paige/azure/App.java
@@ -4,12 +4,6 @@ import com.microsoft.azure.credentials.MSICredentials;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.net.HttpURLConnection;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.Map;
 
