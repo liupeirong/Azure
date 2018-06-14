3fcaab65d0b3a0abf043522718fdc5b3ce1c6423
liupeirong <liupeirong@gmail.com>
2018-06-13 19:36:28 -0700

    workaround for https://github.com/Azure/autorest-clientruntime-for-java/issues/438

--- StorageWithMSI/src/main/java/com/paige/azure/App.java
+++ StorageWithMSI/src/main/java/com/paige/azure/App.java
@@ -29,7 +29,6 @@ public class App
         try
         {
             String storageToken = retrieveMSIToken("https://storage.azure.com/", userAssignedMSIClientId);
-            //String storageToken = retrieveTokenFromIDMS("https://storage.azure.com/", userAssignedMSIClientId);
             System.out.println("token: " + storageToken );
 
             StorageCredentialsToken storageCredentialsToken = 
@@ -57,53 +56,6 @@ public class App
         } catch (Exception ex)
         {
             System.out.println(ex.getMessage());
-        }
-        return token;
-    }
-
-
-    private static String retrieveTokenFromIDMS(String resource, String clientId) {
-        String token = null;
-        HttpURLConnection connection = null;
-
-        try {
-            StringBuilder payload = new StringBuilder();
-
-            payload.append("api-version");
-            payload.append("=");
-            payload.append(URLEncoder.encode("2018-02-01", "UTF-8"));
-            payload.append("&");
-            payload.append("resource");
-            payload.append("=");
-            payload.append(URLEncoder.encode(resource, "UTF-8"));
-            if (clientId != null) {
-                payload.append("&");
-                payload.append("client_id");
-                payload.append("=");
-                payload.append(URLEncoder.encode(clientId, "UTF-8"));
-            }
-
-            URL url = new URL(
-                String.format("http://169.254.169.254/metadata/identity/oauth2/token?%s", 
-                payload.toString()));
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Metadata", "true");
-            connection.connect();
-            InputStream stream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"), 100);
-            String result = reader.readLine();
-            String lookfor = "\"access_token\":\""; 
-            int begin = result.indexOf(lookfor) + lookfor.length();
-            int end = result.indexOf("\"", begin);
-            token = result.substring(begin, end);
-        }
-        catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        } 
-        finally {
-            if (connection != null) 
-                connection.disconnect();
         }
         return token;
     }
